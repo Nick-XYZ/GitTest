a91eed1ad8329b7b38c4250a19d4b98f956c1d43
Nicolas Holst <nicolasholst@gmail.com>
2023-01-23 14:54:12 +0100

    New clone, ändring 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        //Test
-        System.out.println("Task 7");
-        System.out.println("Ändrat i clone");
+        System.out.println("Ändrat i clone 2");
     }
 }
